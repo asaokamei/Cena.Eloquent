d45800c8f5b35544f611571eada84855437a2b2c
Asao <kamei@workspot.jp>
2014-04-12 14:58:50 +0900

    add more tests on EmaEloquent object.

--- tests/Tests/EmaEloquent_BasicTest.php
+++ tests/Tests/EmaEloquent_BasicTest.php
@@ -4,7 +4,6 @@ namespace Tests;
 use Cena\Eloquent\EmaEloquent;
 use Cena\Eloquent\Factory;
 use Illuminate\Database\Eloquent\Model as Eloquent;
-use Illuminate\Support\Collection;
 
 require_once( dirname(__DIR__).'/boot.php' );
 require_once( __DIR__.'/SetUpsTrait.php' );
@@ -37,10 +36,74 @@ class EmaEloquent_BasicTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    function Ema_newEntity_creates_new_entity()
+    function newEntity_creates_new_entity()
     {
         $entity = $this->ema->newEntity( 'Post' );
         $this->assertEquals( 'Post', get_class( $entity ) );
         $this->assertTrue( $entity instanceof Eloquent );
+    }
+
+    /**
+     * @test
+     */
+    function findEntity_returns_Post_entity()
+    {
+        $entity = $this->ema->findEntity( 'Post', 1 );
+        $this->assertEquals( 'Post', get_class( $entity ) );
+        $this->assertTrue( $entity instanceof Eloquent );
+        $this->assertEquals( 'this is Laravel Blog', $entity->title );
+    }
+
+    /**
+     * @test
+     */
+    function getKey_returns_primary_key()
+    {
+        $entity = $this->ema->findEntity( 'Post', 1 );
+        $this->assertEquals( '1', $this->ema->getId( $entity ) );
+        $this->assertEquals( $entity->getKey(), $this->ema->getId( $entity ) );
+    }
+    
+    /**
+     * @test
+     */
+    function save_new_and_retrieved_entities()
+    {
+        // new entity
+        /** @var \Post $new1 */
+        $new1 = $this->ema->newEntity( 'Post' );
+        $new1->title = 'save_new_and_retrieved_entities:new1';
+        $get1 = $this->ema->findEntity( 'Post', 1 );
+        $get1->title = 'save_new_and_retrieved_entities:get1';
+        
+        // save
+        $this->ema->save();
+        
+        // get them from db
+        $new1a = \Post::find( $new1->getKey() );
+        $get1a = \Post::find( $get1->getKey() );
+
+        $this->assertEquals( $new1->title, $new1a->title );
+        $this->assertEquals( $get1->title, $get1a->title );
+        $this->assertNotSame( $new1, $new1a );
+        $this->assertNotSame( $get1, $get1a );
+    }
+
+    /**
+     * @test
+     */
+    function isRetrieved_returns_true_for_entity_from_db()
+    {
+        $entity = $this->ema->findEntity( 'Post', 1 );
+        $this->assertEquals( true, $this->ema->isRetrieved( $entity ) );
+    }
+    
+    /**
+     * @test
+     */
+    function isRetrieved_returns_false_for_new_entity()
+    {
+        $entity = $this->ema->newEntity( 'Post', 1 );
+        $this->assertEquals( false, $this->ema->isRetrieved( $entity ) );
     }
 }
