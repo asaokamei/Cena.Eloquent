e3aeeb2447bc292ae570768763a9552e5922c6a0
Asao <kamei@workspot.jp>
2014-04-16 07:03:06 +0900

    add CenaManager test as part of test-suite.

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -7,6 +7,7 @@ class AllTest
         $suite = new PHPUnit_Framework_TestSuite( 'all tests' );
         $suite->addTestFile( __DIR__ . '/Tests/Eloquent_BasicTest.php' );
         $suite->addTestFile( __DIR__ . '/Tests/EmaEloquent_basicTest.php' );
+        $suite->addTestFile( __DIR__ . '/Tests/Cm_basicTest.php' );
         return $suite;
     }
 }
